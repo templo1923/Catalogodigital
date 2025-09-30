6f6e8c512c3e9dbc85fad85e79f30e6b283d92fe
templo1923 <temploluxury@gmail.com>
2025-09-30 16:21:54 -0500

    rt

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import IndexLayout from "../Layouts/IndexLayout";
+import MainLayout from "../Layouts/MainLayout";
+import PagesLayaut from '../Layouts/PagesLayaut'
+import { createBrowserRouter } from "react-router-dom";
+import Productos from '../Pages/Productos/Productos'
+import Usuarios from '../Pages/Usuarios/Usuarios'
+import Banners from "./Banners/Banners";
+import Main from "./Main/Main";
+import Contacto from "./Contacto/Contacto";
+import Categorias from "./Categorias/Categorias";
+import Codigos from "./Codigos/Codigos";
+import Mesas from "./Mesas/Mesas";
+import Pedidos from "./Pedidos/Pedidos";
+import PageDetail from '../Pages/PageDetail/PageDetail';
+export const router = createBrowserRouter([
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    {
+        path: "/",
+        element: <IndexLayout />,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    },
+    {
+        path: "/",
+        element: <PagesLayaut />,
+        children: [
+            {
+                path: `/producto/:idProducto/:producto`,
+                element: <PageDetail />,
+            },
+
+        ]
+    },
+
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                path: `/dashboard`,
+                element: <Main />,
+            },
+            {
+                path: `/dashboard/productos`,
+                element: <Productos />,
+            },
+            {
+                path: `/dashboard/usuarios`,
+                element: <Usuarios />,
+            },
+            {
+                path: `/dashboard/banners`,
+                element: <Banners />,
+            },
+            {
+                path: `/dashboard/contacto`,
+                element: <Contacto />,
+            },
+            {
+                path: `/dashboard/categorias`,
+                element: <Categorias />,
+            },
+            {
+                path: `/dashboard/codigos`,
+                element: <Codigos />,
+            },
+            {
+                path: `/dashboard/mesas`,
+                element: <Mesas />,
+            },
+            {
+                path: `/dashboard/pedidos`,
+                element: <Pedidos />,
+            },
+        ],
+    },
+
+
+]);
