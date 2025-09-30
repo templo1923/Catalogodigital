c773a8967d533ebd8a6454e70968301fe21da649
templo1923 <temploluxury@gmail.com>
2025-09-30 09:26:51 -0500

    priemra

---
+++ src/Components/Developer/Developer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Developer.css';
+import { Link as Anchor } from 'react-router-dom';
+export default function Developer() {
+
+
+    return (
+        <p className='Developer'>
+            © Copyright 2025 Contacto 573004085041
+            Catalogosco.pro
+        </p>
+    );
+}
